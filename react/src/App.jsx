b9443a96b0df7e2972ccaa1dec62de5fbfb681ef
Charan Y <racha.coder@gmail.com>
2024-10-15 10:52:54 +0530

    functional and class components

---
+++ react/src/App.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+// function App() {
+//   return (
+//     <div>This is a functional component</div>
+//   )
+// }
+
+// function Test() {
+//   return (
+//     <h2>This is a Functional Test Component</h2>
+//   )
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is Class component</h2>
+        <Test />
+      </div>
+    )
+  }
+
+}
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is the Test Class Component</h2>
+      </div>
+    )
+  }
+}
+export default App;
